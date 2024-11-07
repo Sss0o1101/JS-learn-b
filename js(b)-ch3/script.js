236c6ef64e7dd7712d8e891572c29bb8f86c3eb6
alias <matusoyo2436@gmail.com>
2024-11-07 23:52:14 +0900

    added new text

--- js(b)-ch3/script.js
+++ js(b)-ch3/script.js
@@ -9,31 +9,44 @@ querySelector・・・HTMLから任意のidがついた要素を取得する
 /* 3-4 -------------------------------------------------------------*/
 // カラーコード : #000000
 
-console.log(document.querySelector('#colorPicker').value);
-document.querySelector('#colorText').textContent = 'カラーコード：';
+    console.log(document.querySelector('#colorPicker').value);
+    document.querySelector('#colorText').textContent = 'カラーコード：';
 
 /* 3-6 -------------------------------------------------------------*/
-console.log(document.querySelector('#colorPicker').value);
-document.querySelector('#colorText').textContent = document.querySelector('#colorPicker').value;
+    console.log(document.querySelector('#colorPicker').value);
+    document.querySelector('#colorText').textContent = document.querySelector('#colorPicker').value;
 
-console.log(document.querySelector('#colorPicker').value);
-document.querySelector('#colorText').textContent = `カラーコード : ${document.querySelector('#colorPicker').value}`;
-
-
-/* 3-7 -------------------------------------------------------------*/
-const names = "sss";
-console.log(names);
-
-//-------------------
-const text = document.querySelector("#colorText");
-const color = document.querySelector("#colorPicker");
-
-text.textContent =`カラーコード : ${color.value}`;
+    console.log(document.querySelector('#colorPicker').value);
+    document.querySelector('#colorText').textContent = `カラーコード : ${document.querySelector('#colorPicker').value}`;
 
 
+/* 3-7 定数 -------------------------------------------------------------*/
+    // const names = "sss";
+    // console.log(names);
 
-/* 3-8 -------------------------------------------------------------*/
-//Color.addEventListener(選択されたら,カラーコードを表示する);
+    // //-------------------
+    const text = document.querySelector("#colorText");
+    const color = document.querySelector("#colorPicker");
 
-const Color = document.querySelector('#colorPicker');
-Color.addEventListener('input', colorBg);
+    text.textContent =`カラーコード : ${color.value}`;
+
+
+/* 3-8 addEventListener -------------------------------------------------------------*/
+
+    //何が.addEventListener（どうなったら,どうなる）;
+    //Color.addEventListener(選択されたら,カラーコードを表示する);
+
+    const Color = document.querySelector('#colorPicker');
+    Color.addEventListener('input', colorBg);
+
+
+/* 3-9 関数 -------------------------------------------------------------*/
+    const text = document.querySelector("#colorText");
+    const colors = document.querySelector("#colorPicker");
+
+    const colorBg = () => {
+        text.textContent =`カラーコード${colors.value}`;
+    };
+
+    //カラーピッカーが変更されたら colorBg を発動させる
+    colors.addEventListener('input', colorBg);       // 3-7,3-8をコメント化
