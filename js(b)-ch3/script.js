3198c27bc22bd9bbe4fa151fd7b0ad93b836b4c6
alias <matusoyo2436@gmail.com>
2024-11-07 21:46:17 +0900

    added new text

--- js(b)-ch3/script.js
+++ js(b)-ch3/script.js
@@ -1,4 +1,11 @@
+/*
+querySelector・・・HTMLから任意のidがついた要素を取得する
+*/
+
 // console.log("準備完了");
 
+// console.log(document.querySelector('#colorPicker').value);
+// document.querySelector('#colorText').textContent = 'カラーコード：';
+
 console.log(document.querySelector('#colorPicker').value);
-document.querySelector('#colorText').textContent = 'カラーコード：'
+document.querySelector('#colorText').textContent = document.querySelector('#colorPicker').value;
