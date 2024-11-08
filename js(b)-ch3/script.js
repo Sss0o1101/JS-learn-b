7271e3ff7982cbe2b39632ac209f83379947c46c
alias <matusoyo2436@gmail.com>
2024-11-08 14:18:27 +0900

    added new text

--- js(b)-ch3/script.js
+++ js(b)-ch3/script.js
@@ -9,15 +9,15 @@ querySelector・・・HTMLから任意のidがついた要素を取得する
 /* 3-4 -------------------------------------------------------------*/
 // カラーコード : #000000
 
-    console.log(document.querySelector('#colorPicker').value);
-    document.querySelector('#colorText').textContent = 'カラーコード：';
+    // console.log(document.querySelector('#colorPicker').value);
+    // document.querySelector('#colorText').textContent = 'カラーコード：';
 
 /* 3-6 -------------------------------------------------------------*/
-    console.log(document.querySelector('#colorPicker').value);
-    document.querySelector('#colorText').textContent = document.querySelector('#colorPicker').value;
+    // console.log(document.querySelector('#colorPicker').value);
+    // document.querySelector('#colorText').textContent = document.querySelector('#colorPicker').value;
 
-    console.log(document.querySelector('#colorPicker').value);
-    document.querySelector('#colorText').textContent = `カラーコード : ${document.querySelector('#colorPicker').value}`;
+    // console.log(document.querySelector('#colorPicker').value);
+    // document.querySelector('#colorText').textContent = `カラーコード : ${document.querySelector('#colorPicker').value}`;
 
 
 /* 3-7 定数 -------------------------------------------------------------*/
@@ -40,30 +40,68 @@ querySelector・・・HTMLから任意のidがついた要素を取得する
     // color.addEventListener('input', colorBg);
 
 
-/* 3-9 関数 -------------------------------------------------------------*/
+/* 3-9 関数 「カラーコード：」というテキストに続いて、選択した色のカラーコードを表示-------------------------------------------------------------*/
+
     // const text = document.querySelector("#colorText");
     // const colors = document.querySelector("#colorPicker");
 
     // const colorBg = () => {
+           //カラーコードを表示
     //     text.textContent =`カラーコード${colors.value}`;
     // };
 
     // //カラーピッカーが変更されたら colorBg を発動させる
-    // colors.addEventListener('input', colorBg);       // 3-7,3-8をコメント化
+    // colors.addEventListener('input', colorBg);       // 全てコメント化
 
 
 /* 3-10 カラーピッカーが選んだ色を背景に反映させる -------------------------------------------------------------*/
 
+    // const text = document.querySelector('#colorText');
+    // const color = document.querySelector('#colorPicker')
+
+    // //カラーピッカーを操作した時の一連の動作
+    // const colorBg = () => {
+    //     //選択した色を背景色に指定
+    //     document.body.style.backgroundColor = color.value;
+    //     //カラーコードを表示
+    //     text.textContent = `カラーコード : ${color.value}`;
+    // };
+
+    // //カラーピッカーが変更されたら colorBg を発動させる
+    // color.addEventListener("input" , colorBg);      // 全てコメント化
+
+
+/* 3-11 条件分岐 -------------------------------------------------------------*/
     const text = document.querySelector('#colorText');
-    const color = document.querySelector('#colorPicker')
+    const color =document.querySelector('#colorPicker');
 
     //カラーピッカーを操作した時の一連の動作
     const colorBg = () => {
         //選択した色を背景色に指定
-        document.body.style.backgroundColor = color.value
+        document.body.style.backgroundColor = color.value;
         //カラーコードを表示
-        text.textContent = `カラーコード : ${color.valuue}`;
+        if (color.value === "#ffffff" ) {
+            text.textContent = `カラーコード：${color.value} (white)`;
+        }
     };
 
     //カラーピッカーが変更されたら colorBg を発動させる
-    color.addEventListener("input" , colorBg);
+    color.addEventListener('input', colorBg);        // 全てコメント化
+
+
+/* 3-11(2) 条件分岐else -------------------------------------------------------------*/
+    const text = document.querySelector('#colorText');
+    const color =document.querySelector('#colorPicker');
+
+    //カラーピッカーを操作した時の一連の動作
+    const colorBg = () => {
+        //選択した色を背景色に指定
+        document.body.style.backgroundColor = color.value;
+        //カラーコードを表示
+        if (color.value === "#ffffff" ) {
+            text.textContent = `カラーコード：${color.value} (white)`;
+        }
+    };
+
+    //カラーピッカーが変更されたら colorBg を発動させる
+    color.addEventListener('input', colorBg);        // 全てコメント化
