fdb6d6aa4d7cdedf7d65e1ce6843cffe0bddb605
alias <matusoyo2436@gmail.com>
2024-11-08 13:41:33 +0900

    aded new text

--- js(b)-ch3/script.js
+++ js(b)-ch3/script.js
@@ -25,28 +25,45 @@ querySelector・・・HTMLから任意のidがついた要素を取得する
     // console.log(names);
 
     // //-------------------
-    const text = document.querySelector("#colorText");
-    const color = document.querySelector("#colorPicker");
+    // const text = document.querySelector("#colorText");
+    // const color = document.querySelector("#colorPicker");
 
-    text.textContent =`カラーコード : ${color.value}`;
+    // text.textContent =`カラーコード : ${color.value}`;
 
 
 /* 3-8 addEventListener -------------------------------------------------------------*/
 
     //何が.addEventListener（どうなったら,どうなる）;
-    //Color.addEventListener(選択されたら,カラーコードを表示する);
+    //color.addEventListener(選択されたら,カラーコードを表示する);
 
-    const Color = document.querySelector('#colorPicker');
-    Color.addEventListener('input', colorBg);
+    // const Color = document.querySelector('#colorPicker');
+    // color.addEventListener('input', colorBg);
 
 
 /* 3-9 関数 -------------------------------------------------------------*/
-    const text = document.querySelector("#colorText");
-    const colors = document.querySelector("#colorPicker");
+    // const text = document.querySelector("#colorText");
+    // const colors = document.querySelector("#colorPicker");
 
+    // const colorBg = () => {
+    //     text.textContent =`カラーコード${colors.value}`;
+    // };
+
+    // //カラーピッカーが変更されたら colorBg を発動させる
+    // colors.addEventListener('input', colorBg);       // 3-7,3-8をコメント化
+
+
+/* 3-10 カラーピッカーが選んだ色を背景に反映させる -------------------------------------------------------------*/
+
+    const text = document.querySelector('#colorText');
+    const color = document.querySelector('#colorPicker')
+
+    //カラーピッカーを操作した時の一連の動作
     const colorBg = () => {
-        text.textContent =`カラーコード${colors.value}`;
+        //選択した色を背景色に指定
+        document.body.style.backgroundColor = color.value
+        //カラーコードを表示
+        text.textContent = `カラーコード : ${color.valuue}`;
     };
 
     //カラーピッカーが変更されたら colorBg を発動させる
-    colors.addEventListener('input', colorBg);       // 3-7,3-8をコメント化
+    color.addEventListener("input" , colorBg);
