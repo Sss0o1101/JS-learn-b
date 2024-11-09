8adf6d131ab8cfae4164d583e0ce09412aa06a94
alias <matusoyo2436@gmail.com>
2024-11-09 23:52:55 +0900

    added new text

--- js(b)-ch4-(10,11,12)/script.js
+++ js(b)-ch4-(10,11,12)/script.js
@@ -1,15 +1,18 @@
 /* 4-10 チェックでボタンを有効化 ------------------------------------------------------------------------------------------*/
-const isAgreed = document.querySelector('#check');
-const btn = document.querySelector('#btn');
 
-isAgreed.addEventListener('change', () => {
-    //チェックボックスにチェックが入っているなら
-    if (isAgreed.checked === true) {
-        btn.disabled = false;  //ボタンの無効化を無しにする
-    } else {
-        btn.disabled = true;   //ボタンを無効化にする
-    }
-});
+    //<input id="btn" disabled> disabledの有無 (classではない)
+
+    const isAgreed = document.querySelector('#check');
+    const btn = document.querySelector('#btn');
+
+    isAgreed.addEventListener('change', () => {
+        //チェックボックスにチェックが入っているなら
+        if (isAgreed.checked === true) {
+            btn.disabled = false;  //ボタンの無効化を無しにする
+        } else {
+            btn.disabled = true;   //ボタンを無効化にする
+        }
+    });
 
 //console.log("チェックされました");  メモ
 // console.log(isAgreed.checked);  メモ
