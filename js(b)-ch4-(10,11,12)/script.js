65231738dbc63d13e6440b0f2e7272f4ecf47261
alias <matusoyo2436@gmail.com>
2024-11-09 23:47:26 +0900

    aded new text

--- js(b)-ch4-(10,11,12)/script.js
+++ js(b)-ch4-(10,11,12)/script.js
@@ -1,3 +1,15 @@
 /* 4-10 チェックでボタンを有効化 ------------------------------------------------------------------------------------------*/
 const isAgreed = document.querySelector('#check');
 const btn = document.querySelector('#btn');
+
+isAgreed.addEventListener('change', () => {
+    //チェックボックスにチェックが入っているなら
+    if (isAgreed.checked === true) {
+        btn.disabled = false;  //ボタンの無効化を無しにする
+    } else {
+        btn.disabled = true;   //ボタンを無効化にする
+    }
+});
+
+//console.log("チェックされました");  メモ
+// console.log(isAgreed.checked);  メモ
