749549426ef9e763ee14442e48a1c1ca2500e8de
alias <matusoyo2436@gmail.com>
2024-11-10 23:32:16 +0900

    added new text

--- JS-learn(b)-(ch5.6)/js(b)-ch5-(1)/script.js
+++ JS-learn(b)-(ch5.6)/js(b)-ch5-(1)/script.js
@@ -1,8 +1,32 @@
 /* 5-1 作成する画像一覧ページの紹介 ------------------------------------------------------------------------------------------*/
+    //insertAdjacentHTMLの説明
 
-    const menu = document.querySelector('#menu');
-    const content = `<div><img src="./images/strawberry.jpg" alt=""></div>`;
+    // const menu = document.querySelector('#menu');
+    // const content = `<div><img src="./images/strawberry.jpg" alt=""></div>`;
 
-    // menu.textContent = content;   //これだと文字列として表示される
+    // // menu.textContent = content;   //これだと文字列として表示される
+    // menu.insertAdjacentHTML('beforeend', content);   //これならstrawberry.jpgの画像が表示される
 
-    menu.insertAdjacentHTML('beforeend', content)   //これならstrawberry.jpgの画像が表示される
+/* 5-2-----------------------------------------------------------------------------------------------------------------*/
+
+/* 5-3 配列で複数の画像ファイル名をまとめよう---------------------------------------------------------------------------------*/
+
+    // const menu = document.querySelector('#menu');
+    // const lists = [
+    //     'strawberry.jpg',
+    //     'lime.jpg',
+    //     'mango.jpg',
+    //     'lemon.jpg',
+    //     'fig.jpg',
+    //     'apple.jpg',
+    // ];
+
+    // console.log(lists);
+
+
+    // const content = `<div><img src="./images/strawberry.jpg" alt=""></div>`;
+    // menu.insertAdjacentHTML('beforeend', content);
+
+/* ---------------------------------------------------------------------------------------------------------------------*/
+
+/* 5-4 配列の中にある画像を表示しよう---------------------------------------------------------------------------------------------------------------*/
