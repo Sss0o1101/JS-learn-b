180dc50eebd73d52b241b194297fb85ce8f7d3bf
alias <matusoyo2436@gmail.com>
2024-11-10 14:24:01 +0900

    added new text

--- JS-learn(b)-(box)/js(b)-ch4-(2,3)/script.js
+++ JS-learn(b)-(box)/js(b)-ch4-(2,3)/script.js
@@ -17,7 +17,6 @@
 
 
 
-
 /* 4-4 ボタンをクリックしてダークモード () -------------------------------------------------------------*/
     const btn = document.querySelector('#btn');
 
@@ -52,7 +51,6 @@
 
 
 
-
 /* 4-7 入力した文字数を数えてみよう (length) -------------------------------------------------------------*/
 
     const text = document.querySelector('#text');
@@ -83,3 +81,50 @@
 
 
 /* 4-9 if else ------------------------------------------------------------------------------*/
+
+
+/* 4-10 チェックでボタンを有効化 ------------------------------------------------------------------------------------------*/
+
+    //<input id="btn" disabled> disabledの有無 (classではない)
+
+    const isAgreed = document.querySelector('#check');
+    const btn = document.querySelector('#btn');
+
+    isAgreed.addEventListener('change', () => {
+        //チェックボックスにチェックが入っているなら
+        if (isAgreed.checked === true) {
+            btn.disabled = false;  //ボタンの無効化を無しにする
+        } else {
+            btn.disabled = true;   //ボタンを無効化にする
+        }
+    });
+
+    //console.log("チェックされました");  メモ
+    // console.log(isAgreed.checked);  メモ
+
+
+/* 4-11 別の書き方(省略) ------------------------------------------------------------------------------------------*/
+
+    const isAgreed = document.querySelector('#check');
+    const btn = document.querySelector('#btn');
+
+    isAgreed.addEventListener('change', () => {
+        if (isAgreed.checked) {
+            btn.disabled = false;
+        } else {
+            btn.disabled = true;
+        }
+    });
+
+/* 4-11 別の書き方(省略) ! ------------------------------------------------------------------------------------------*/
+
+    // isAgreedがtrueだから !はfalseになる
+
+    const isAgreed = document.querySelector('#check');
+    const btn = document.querySelector('#btn');
+
+    isAgreed.addEventListener('change', () => {
+        btn.disabled = !isAgreed.checked;
+    });
+
+/* ----------------------------------------------------------------------------------------------------------------*/
