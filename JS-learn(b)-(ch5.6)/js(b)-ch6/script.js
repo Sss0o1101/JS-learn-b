913af802d614bda732269ecf4eb214038bb1b86d
alias <matusoyo2436@gmail.com>
2024-11-12 17:52:29 +0900

    added new text

--- JS-learn(b)-(ch5.6)/js(b)-ch6/script.js
+++ JS-learn(b)-(ch5.6)/js(b)-ch6/script.js
@@ -52,17 +52,45 @@
 
 /* 6-8(1) 色々な見出しのアニメーション 回転 ------------------------------------------------------------------------------------------*/
 
-    const heading = document.querySelector('#heading');
+    // const heading = document.querySelector('#heading');
+
+    // const keyframes = {
+    //     opacity: [0, 1],
+    //     rotate: ['x 360deg', 0],   //回転
+    // };
+    // const options = {
+    //     duration: 1000,
+    //     easing: 'ease',
+    // }
+
+    // heading.animate(keyframes, options);
+
+/* 6-8(2) 色が変化する見出しを作る -------------------------------------------*/
+
+    // const heading = document.querySelector('#heading');
+
+    // const keyframes = {
+    //     color: ['#f66', '#fc2', '#0c6', '#0bd']   //文字色を4色用意
+    // };
+    // const options = {
+    //     duration: 8000,
+    //     direction: 'alternate',   //終了地点に来たら開始地点に戻す
+    //     iterations: Infinity,      //ずっと繰り返す
+    // };
+
+    // heading.animate(keyframes, options);
+
+/* 6-8(3) 背景が伸びる見出しを作る -------------------------------------------*/
+
+const heading = document.querySelector('#heading');
 
     const keyframes = {
-        opacity: [0, 1],
-        rotate: ['x 360deg', 0],   //回転
+        color: ['transparent', '#fff'],          //文字を透明から白に
+        backgroundPosition: ['100% 0', '0 0'],   //背景の位置を動かして四角形が伸びた様に見える
     };
     const options = {
         duration: 1000,
-        easing: 'ease',
-    }
+        easing: 'ease'
+    };
 
     heading.animate(keyframes, options);
-
-/* 6-8(2) ------------------------------------------------------*/
