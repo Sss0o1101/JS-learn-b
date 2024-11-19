819f39f8eb57a34cb1410123c8a8c73be01eadf1
alias <matusoyo2436@gmail.com>
2024-11-19 16:06:01 +0900

    added new text

--- JS-learn(b)-(ch7)/js(b)-ch7-()/js/script.js
+++ JS-learn(b)-(ch7)/js(b)-ch7-()/js/script.js
@@ -53,3 +53,18 @@ window.addEventListener('load', () => {
     );
 
 });
+
+
+/*
+画像ギャラリー
+================================================ */
+const mainImage = document.querySelector('.gallery-image img');
+const thumbImage = document.querySelectorAll('.gallery-thumbnails img');
+
+for (let i = 0; i < thumbImage.length; i++) {
+    thumbImage[i].addEventListener('mouseover', (event) => {
+        mainImage.src = event.target.src;
+        mainImage.animate({opacity: [0, 1]}, 500);
+    });
+
+}
