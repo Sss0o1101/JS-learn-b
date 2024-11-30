c6a0ac0d40898699bd2c1b1aced250581defdea4
alias <matusoyo2436@gmail.com>
2024-11-30 14:17:35 +0900

    added new text

--- JS-learn(b)-(ch5.6)/js(b)-ch6-(13,14,15,16)/script.js
+++ JS-learn(b)-(ch5.6)/js(b)-ch6-(13,14,15,16)/script.js
@@ -1,35 +1,35 @@
 /* 6-13.14 (IntersectionObserver)の仕組み  ------------------------------------------------------------------------------------------*/
 
-    // //監視対象が範囲内に現れたら実行する動作
-    // const showKirin = () => {
-    //     console.log("キリンさんです");
-    // };
+    //監視対象が範囲内に現れたら実行する動作
+    const showKirin = () => {
+        console.log("キリンさんです");
+    };
 
-    // //監視ロボットの設定
-    // const kirinObserver = new IntersectionObserver(showKirin);   //関数名を設定、動作内容を指示する
+    //監視ロボットの設定
+    const kirinObserver = new IntersectionObserver(showKirin);   //関数名を設定、動作内容を指示する
 
-    // //kirinを監視するよう指示
-    // kirinObserver.observe(document.querySelector('#kirin'));
+    //kirinを監視するよう指示
+    kirinObserver.observe(document.querySelector('#kirin'));
 
 /* ----------------------------------------------------------------------------------------------------------------------------------------------*/
 
 /* 6-15 交差状態の情報を見てみよう  ------------------------------------------------------------------------------------------*/
 
-    // //監視対象が範囲内に現れたら実行する動作
-    // const showKirin = (entries) => {
-    //     console.log(entries[0].target);   //[IntersectionObserverEntry]という配列が取得できた。
-    // };
+    //監視対象が範囲内に現れたら実行する動作
+    const showKirin = (entries) => {
+        console.log(entries[0].target);   //[IntersectionObserverEntry]という配列が取得できた。
+    };
 
-    // //監視ロボットの設定
-    // const kirinObserver = new IntersectionObserver(showKirin);   //関数名を設定、動作内容を指示する
+    //監視ロボットの設定
+    const kirinObserver = new IntersectionObserver(showKirin);   //関数名を設定、動作内容を指示する
 
-    // //kirinを監視するよう指示
-    // kirinObserver.observe(document.querySelector('#kirin'));
+    //kirinを監視するよう指示
+    kirinObserver.observe(document.querySelector('#kirin'));
 
 
-    // // IntersectionObserver オブジェクトプロパティ
-    // //boundingClientRect  //intersectionRect  //intersectionRatio
-    // //isIntersecting  //rootBounds  //target  //time
+    // IntersectionObserver オブジェクトプロパティ
+    //boundingClientRect  //intersectionRect  //intersectionRatio
+    //isIntersecting  //rootBounds  //target  //time
 
 /* ----------------------------------------------------------------------------------------------------------------------------------------------*/
 
