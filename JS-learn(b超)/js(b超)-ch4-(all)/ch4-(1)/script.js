885848bcd97e9614af31056875535948d5d524d4
alias <matusoyo2436@gmail.com>
2024-12-05 14:30:47 +0900

    added new text

--- JS-learn(b超)/js(b超)-ch4-(all)/ch4-(1)/script.js
+++ JS-learn(b超)/js(b超)-ch4-(all)/ch4-(1)/script.js
@@ -1,9 +1,9 @@
 /* 4-1 (入力内容の取得とイベント)  ------------------------------------------------------------------------------------------*/
     //.onsubmit = function () {};
 
-    document.querySelector('#form').onsubmit = function () {
-        console.log('クリックされました');
-    };
+    // document.querySelector('#form').onsubmit = function () {
+    //     console.log('クリックされました');
+    // };
 
     // //別の書き方
     // const submit = document.querySelector('#form');
@@ -11,3 +11,15 @@
     //     event.preventDefault(); // フォームのデフォルトの送信を防ぐ
     //     console.log('クリックされました');
     // });
+
+/* ----------------------------------------------------------------------------------------------------------------------------------------------*/
+/* 4-1 (2)  -------------------------------------------------------------------------------------------------------------------------------------*/
+    //event.preventDefault();
+
+    document.querySelector('#form').onsubmit = function (event) {
+        event.preventDefault();  //action属性で指定されたページに移動を
+        const search = document.querySelector('#form').word.value;
+        document.querySelector('#output').textContent = `「${search}の検索中...`;
+    };
+
+/* ----------------------------------------------------------------------------------------------------------------------------------------------*/
