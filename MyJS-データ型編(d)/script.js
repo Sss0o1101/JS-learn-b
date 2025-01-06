0fd3ddb6c8e0ebbe0439ca06473a07e8a4bd59b9
alias <matusoyo2436@gmail.com>
2025-01-06 22:48:58 +0900

    added new text

--- MyJS-データ型編(d)/script.js
+++ MyJS-データ型編(d)/script.js
@@ -1,2 +1,29 @@
-/*   ------------------------------------------------------------------------------------------*/
+/*
+二択のデータを扱う際に便利な真偽値について
+!で真偽値を反転させよう------------------------------------------------------------------------------------------*/
 
+  // true false
+  // let mode = 'dev';
+  // let mode = 'normal';
+  // let isDevMode = true;
+  // let isDevMode = false;
+
+  let isDevMode = true;
+  const pElement = document.querySelector('p');
+  const btnElement = document.querySelector('button');
+
+  btnElement.addEventListener('click', () => {
+    if (isDevMode === true) {
+      pElement.textContent = 'Dev Mode is Off.';
+      // isDevMode =false;
+      // isDevMode = !isDevMode;  //同じ意味
+    } else {
+      pElement.textContent = 'Dev Mode is On.';
+      // isDevMode === true;
+      // isDevMode = !isDevMode;  //同じ意味
+    }
+
+    isDevMode = !isDevMode        //外に書いても同じ意味
+  });
+
+/* ----------------------------------------------------------------------------------------------------------------------------------------------*/
