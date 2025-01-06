8034dd7e566f4295430ba0612f72320c15c7ac3e
alias <matusoyo2436@gmail.com>
2025-01-06 23:08:51 +0900

    added new text

--- MyJS-データ型編(d)/script.js
+++ MyJS-データ型編(d)/script.js
@@ -8,22 +8,46 @@
   // let isDevMode = true;
   // let isDevMode = false;
 
+  {
+
+    let isDevMode = true;
+    const pElement = document.querySelector('p');
+    const btnElement = document.querySelector('button');
+
+    btnElement.addEventListener('click', () => {
+      if (isDevMode === true) {
+        pElement.textContent = 'Dev Mode is Off.';
+        // isDevMode =false;
+        // isDevMode = !isDevMode;  //同じ意味
+      } else {
+        pElement.textContent = 'Dev Mode is On.';
+        // isDevMode === true;
+        // isDevMode = !isDevMode;  //同じ意味
+      }
+
+      isDevMode = !isDevMode        //外に書いても同じ意味
+    });
+
+  }
+/* ----------------------------------------------------------------------------------------------------------------------------------------------*/
+
+/* ifの条件分岐では真偽値が期待されているので、真偽値そのものを渡して処理する方法 ------------------------------------------------------------------------------------------*/
+
+
+
   let isDevMode = true;
   const pElement = document.querySelector('p');
   const btnElement = document.querySelector('button');
 
   btnElement.addEventListener('click', () => {
-    if (isDevMode === true) {
-      pElement.textContent = 'Dev Mode is Off.';
-      // isDevMode =false;
-      // isDevMode = !isDevMode;  //同じ意味
-    } else {
-      pElement.textContent = 'Dev Mode is On.';
-      // isDevMode === true;
-      // isDevMode = !isDevMode;  //同じ意味
+    if(confirm('Are you sure?') === true) {
+      if (isDevMode) {
+        pElement.textContent = 'Dev Mode is Off.';
+      } else {
+        pElement.textContent = 'Dev Mode is On.';
+      }
+      isDevMode = !isDevMode
     }
-
-    isDevMode = !isDevMode        //外に書いても同じ意味
   });
 
 /* ----------------------------------------------------------------------------------------------------------------------------------------------*/
