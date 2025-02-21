47cadb87443da81aa4c83c0653e52d0694e9ad20
alias <matusoyo2436@gmail.com>
2025-02-21 23:18:40 +0900

    added new text

--- MyJS-クラス編(y)/script.js
+++ MyJS-クラス編(y)/script.js
@@ -59,3 +59,12 @@
 /* ----------------------------------------------------------------------------------------------------------------------------------------------*/
 
 /*  ------------------------------------------------------------------------------------------*/
+
+  const $elm = document.querySelector('#js-accordion');
+  const $trigger = $elm.getElementsByTagName('a');
+
+  $trigger[0].addEventListener('click', (e) => clickHandler(e));
+  const clickHandler = (e) => {
+    e.preventDefault();
+    console.log('クリックされました');
+  }
